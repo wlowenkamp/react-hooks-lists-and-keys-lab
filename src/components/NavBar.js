5a549cc2cbc47bc1f85cadc1fd7105f2640e5a76
Will Lowenkamp <lowenkamp97@gmail.com>
2023-06-14 16:34:16 -0400

    complete

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,12 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
-
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  
+  return <nav>
+    <a key = {"home"} href= "#home">home</a> 
+    <a key = {"about"} href= "#about">about</a>
+    <a key = {"propjects"} href= "#projects">projects</a>
+    </nav>;
 }
 
 export default NavBar;
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,12 +1,24 @@
 import React from "react";
 import ProjectItem from "./ProjectItem";
+import User from "../data/user"
+
 
 function ProjectList({ projects }) {
   console.log(projects);
+
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        {projects.map((project) => (
+          <ProjectItem
+            key={project.id}
+            name={project.name}
+            about={project.about}
+            technologies={project.technologies}
+          />
+        ))}
+      </div>
     </div>
   );
 }
